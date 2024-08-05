8bb36d9547823af410e0f5077f9b8dab964cd066
raghu <simu.singh2504@gmail.com>
2024-08-05 14:47:50 +0530

    node module deleted

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+const cors = require('cors')
+const PORT= 3000
+
+const userData = [
+{
+    name: "Raghvendra",
+    course: "B.Tech CSE",
+    sec: "B",
+    batch: "2022-2026",
+},
+{
+    name: "Amay",
+    course: "B.Tech CSE",
+    sec: "B",
+    batch: "2022-2026",
+}]
+
+app.get("/",(req,res)=>{
+    res.json(userData)
+})
+
+
+app.listen(PORT,(err)=>{
+    if(!err) console.log("Server is running at ",PORT)
+})
+
