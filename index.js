b2d57a19d2fcfca1def54f10fc4f9e45abef487a
Raghvendra2504 <simu.singh2504@gmail.com>
2024-08-05 16:16:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ const userData = [
     course: "B.Tech CSE",
     sec: "B",
     batch: "2022-2026",
-}]
+},
+{
+    name: "Baqir Rizvi",
+    course: "B.Tech CSE",
+    sec: "B",
+    batch: "2022-2026",
+},
+]
 
 app.get("/",(req,res)=>{
     res.json(userData)
